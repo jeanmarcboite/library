8b4cdbba77143491fcba224cbd7be6c2de2c5dc8
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-12-23 16:19:50 +0100

    remove unused

--- frontend/src/components/Header/Dropdown.svelte
+++ frontend/src/components/Header/Dropdown.svelte
@@ -5,22 +5,6 @@
 </script>
 
 <style>
-    /* since nested groupes are not supported we have to use 
-       regular css for the nested dropdowns 
-    */
-    li > ul {
-        transform: translatex(100%) scale(0);
-    }
-    li:hover > ul {
-        transform: translatex(101%) scale(1);
-    }
-    li > button svg {
-        transform: rotate(-90deg);
-    }
-    li:hover > button svg {
-        transform: rotate(-270deg);
-    }
-
     /* Below styles fake what can be achieved with the tailwind config
        you need to add the group-hover variant to scale and define your custom
        min width style.
@@ -29,9 +13,6 @@
     */
     .group:hover .group-hover\:scale-100 {
         transform: scale(1);
-    }
-    .group:hover .group-hover\:-rotate-180 {
-        transform: rotate(180deg);
     }
     .scale-0 {
         transform: scale(0);
