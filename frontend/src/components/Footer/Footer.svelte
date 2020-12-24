1913a88ff3dfa6b26ef39a396a285b56106cadfb
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-12-24 11:01:02 +0100

    copyright

--- frontend/src/components/Footer/Footer.svelte
+++ frontend/src/components/Footer/Footer.svelte
@@ -3,12 +3,12 @@
     AppInfo().then((info) => (appInfo = info));
 </script>
 
-<div class="flex flex-row text-white">
-    <div class="block m-2">left</div>
+<div class="flex flex-row text-gray-500">
+    <div class="block m-2">Copyright &copy; 2020 — WTFPL</div>
     <div class="flex-grow" />
     <div class="flex flex-row flex-grow-0 m-2">
         Powered by
-        {appInfo.Name}, version
-        {appInfo.Version}
+        <span class="text-red-600">{appInfo.Name}</span>, version
+        <span class="text-green-600">{appInfo.Version}</span>
     </div>
 </div>
