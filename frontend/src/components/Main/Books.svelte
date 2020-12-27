89ab6033339f9b861bc3fc1001e0acfe75525b7a
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-12-27 18:49:07 +0100

    data table

--- frontend/src/components/Main/Books.svelte
+++ frontend/src/components/Main/Books.svelte
@@ -1,13 +1,44 @@
 <script>
-    import App from "../../App.svelte";
+    import DataTable from "@fouita/data-table";
     import JSONtree from "../JSONTree.svelte";
     export let db;
+    let books = [];
+    const head = {
+        ID: "ID",
+        Title: "Title",
+        Authors: "Author(s)",
+        Date: "Date",
+        Size: "Size (MB)",
+        Rating: "Rating",
+        Tags: "Tags",
+        Series: "Series",
+        Publisher: "Publisher",
+        Published: "Published",
+    };
+    $: if (db && db.Books) {
+        books = [...Object.keys(db.Books)].map((bookID) => {
+            const b = db.Books[bookID];
+            return {
+                ID: b.ID,
+                Title: b.Title,
+                Authors: "Authors",
+                Date: "Date",
+                Rating: "Rating",
+                Tags: "Tags",
+                Series: "Series",
+                Publisher: b.Publishers.map((p) => p.Name).toString(),
+                Size: (b.Data.UncompressedSize / 1000000).toFixed(1),
+                Published: b.Pubdate,
+            };
+        });
+    }
 </script>
 
 {#if db && db.Books}
     <h1>{db.Filename}</h1>
-    {#each [...Object.keys(db.Books)] as bookID}
-        {db.Books[bookID].Title}
-        <JSONtree value={db.Books[bookID]} />
+    {#each books as book}
+        <h2>{book.Title}</h2>
+        <JSONtree value={book} />
     {/each}
+    <DataTable {head} rows={books} hover />
 {/if}
