c66936cab0948e844881166a8d44382c06326328
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-12-29 13:57:30 +0100

    series, publishers

--- frontend/src/components/Main/Books.svelte
+++ frontend/src/components/Main/Books.svelte
@@ -7,11 +7,31 @@
         //return "<i class='fa fa-print'></i>";
         return "<span class='m-1 icon'><svelte:component this={FaInfoCircle} /></span>";
     };
+    //define custom mutator
+    const SizeMutator = function (value, data, type, params, component) {
+        //value - original value of the cell
+        //data - the data for the row
+        //type - the type of mutation occurring  (data|edit)
+        //params - the mutatorParams object from the column definition
+        //component - when the "type" argument is "edit", this contains the cell component for the edited cell, otherwise it is the column component for the column
+        return (data.Data.UncompressedSize / 1000000).toFixed(1);
+        //return value > mutatorParams.threshold; //return the new value for the cell data.
+    };
+
+    const StringArrayMutator = function (value, data, type, params, component) {
+        if (!value) {
+            return "";
+        }
+        if (Array.isArray(value)) {
+            return value.map((p) => p.Name).toString();
+        }
+        return value;
+    };
+
     const columns = [
-        { formatter: "responsiveCollapse", headerSort: false },
         {
             title: "Title",
-            field: "title",
+            field: "Title",
             hozAlign: "left",
             widthGrow: 1,
             editor: "input",
@@ -25,22 +45,36 @@
                     .localeCompare(String(b).toLowerCase());
             },
         },
-        {
-            title: "Comment",
-            field: "Comment",
-            responsive: 10,
-        },
         { title: "Date", field: "Date" },
-        { title: "Size (MB)", field: "Size", hozAlign: "center" },
+        {
+            title: "Size (MB)",
+            field: "Size",
+            hozAlign: "center",
+            mutator: SizeMutator,
+        },
         {
             title: "Rating",
             field: "Rating",
             hozAlign: "center",
         },
         { title: "Tags", field: "Tags", width: 64 },
-        { title: "Series", field: "Series" },
-        { title: "Publisher", field: "Publisher" },
-        { title: "Published", field: "Published" },
+        {
+            title: "Series",
+            field: "Series",
+            mutator: StringArrayMutator,
+        },
+        {
+            title: "Publishers",
+            field: "Publishers",
+            mutator: StringArrayMutator,
+        },
+        {
+            title: "Published",
+            field: "Pubdate",
+            mutator: function (value, data, type, params, component) {
+                return new Date(value).toDateString();
+            },
+        },
     ];
 
     const mapStrings = (array) =>
@@ -49,11 +83,11 @@
     $: if (db && db.Books) {
         data = [...Object.keys(db.Books)].map((bookID) => {
             const b = db.Books[bookID];
+            return b;
             return {
-                id: b.ID,
+                ID: b.ID,
                 title: b.Title,
                 Authors: b.Authors.map((a) => db.Authors[a].Name).toString(),
-                Comment: b.Comment,
                 Date: new Date(b.LastModified.slice(0, 10)).toDateString(),
                 Rating: b.Rating,
                 Tags: b.Tags ? b.Tags.toString() : "",
