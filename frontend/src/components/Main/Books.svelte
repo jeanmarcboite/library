151346b6857b8d06590488e443d09b92434dbf41
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-12-29 18:22:39 +0100

    size

--- frontend/src/components/Main/Books.svelte
+++ frontend/src/components/Main/Books.svelte
@@ -19,6 +19,11 @@
     };
 
     const StringArrayMutator = function (value, data, type, params, component) {
+        //value - original value of the cell
+        //data - the data for the row
+        //type - the type of mutation occurring  (data|edit)
+        //params - the mutatorParams object from the column definition
+        //component - when the "type" argument is "edit", this contains the cell component for the edited cell, otherwise it is the column component for the column
         if (!value) {
             return "";
         }
@@ -89,9 +94,6 @@
             },
         },
     ];
-
-    const mapStrings = (array) =>
-        array ? array.map((p) => p.Name).toString() : "";
 
     $: if (db && db.Books) {
         data = [...Object.keys(db.Books)].map((bookID) => {
