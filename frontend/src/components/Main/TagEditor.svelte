efe4c03c85bd0516df6af4836a3d910471e03ae9
jeanmarcboite <jeanmarc.boite@gmail.com>
2021-01-07 09:02:49 +0100

    remove

--- frontend/src/components/Main/TagEditor.svelte
+++ frontend/src/components/Main/TagEditor.svelte
@@ -9,18 +9,29 @@
 
     const dispatch = createEventDispatcher();
 
+    const update = (s: string) => {
+        if (!datalist.includes(s)) {
+            datalist.push(s);
+        }
+        items = datalist.filter((t) => !tags.includes(t));
+    };
+
     const add = (s: string) => {
         if (!tags.includes(s)) {
             tags.push(s);
             tags = tags;
         }
-        if (!datalist.includes(s)) {
-            datalist.push(s);
-        }
-        items = datalist.filter((t) => !tags.includes(t));
-        console.log(tags, items, datalist);
+        update(s);
         tag = "";
     };
+
+    const remove = (s: string) => {
+        if (tags.includes(s)) {
+            tags = tags.filter((t) => t !== s);
+            update(s);
+        }
+    };
+
     function save() {
         dispatch("save", { tags });
         modalState.close();
@@ -39,13 +50,13 @@
         add(tag);
     };
     const keydown = (event) => {
+        // enter
         if (event.keyCode == 13) {
-            console.log(event.type, event, tag);
             add(tag);
         }
+        // escape
         if (event.keyCode == 27) {
             tag = "";
-            add(tag);
         }
     };
 </script>
@@ -68,8 +79,17 @@
         <div class="flex flex-row mt-4">
             {#each tags as t}
                 <div
-                    class="inline-flex items-center px-3 py-1 ml-4 text-xs font-bold text-green-700 uppercase bg-green-200 rounded-full leading-sm">
-                    {t}
+                    class="relative inline-flex items-center px-3 py-1 ml-4 text-xs font-bold text-green-700 uppercase bg-green-200 rounded-full leading-sm">
+                    <span class="mr-4">{t}</span>
+                    <span class="absolute right-0" on:click={() => remove(t)}>
+                        <svg
+                            class="w-6 h-6 text-red-400 fill-current"
+                            role="button"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"><title>Remove</title>
+                            <path
+                                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                    </span>
                 </div>
             {/each}
         </div>
